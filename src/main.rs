83aa0fcb400da0ff74a2d3d821a046bf24cf76df
Michael Delgado <mdelgado509@gmail.com>
2021-11-05 15:32:26 -0400

    Adds solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the Fibonacci Generator");
+
+    println!("Please input the nth number in the sequence you would like to generate");
+    
+    let mut nth_fib = String::new();
+
+    io::stdin()
+        .read_line(&mut nth_fib)
+        .expect("Failed to read line.");
+
+    let nth_fib: u32 = nth_fib.trim().parse().expect("Please input a number!");
+
+    let result = fibonacci(nth_fib);
+
+    println!("The Fibonacci number is: {}", result)
+}
+
+fn fibonacci(n: u32) -> u32 {
+    match n {
+        1 => 0,
+        2 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2) 
+    }
 }
